482007ca7762f1c8dc9c90a98571e2ed53e76d88
Priyadharshan L <dharsh1727s@gmail.com>
2026-09-05 18:39:53 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -4,6 +4,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
+
 module tb ();
 
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
