dc60c4a378dbc17a03f40f51f7a2b75e81c9fac1
Priyadharshan L <dharsh1727s@gmail.com>
2026-09-05 18:23:32 +0530

    Remove power ports for Gate Level test
    Removed power ports for Gate Level test from user_project instantiation.

--- test/tb.v
+++ test/tb.v
@@ -26,11 +26,6 @@ module tb ();
   // Replace tt_um_example with your module name:
   tt_um_uart user_project (
 
-           // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
